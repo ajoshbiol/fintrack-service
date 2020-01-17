1a39e8c6626a2b2e1f229a732f19e0216a6a9f8e
Josh Biol <ajbiol@gmail.com>
2020-01-16 21:39:46 -0800

    Implemented GET and DELETE.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 response_type = { 'ContentType':'application/json' }
 
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 es = Elasticsearch()
 
 index_name = 'transactions'
@@ -25,8 +25,47 @@ def validate(data):
     if data['classification'] not in classifications:
         raise Exception('Invalid classification.')
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST', 'DELETE'])
 def transactions():
+    if request.method == 'GET':
+        try:
+            ts = request.args.get('ts')
+            id = request.args.get('id')
+            if ts is not None:
+                if id is None:
+                    raise Exception('Missing id.')
+                ts = int(ts)
+
+            size = request.args.get('size')
+            if size is not None:
+                size = int(size)
+            else:
+                size = 25
+            
+        except Exception as e:
+            return json.dumps({'message' : str(e)}), 400, response_type
+
+        query = {
+            "size": size,
+            "query": {
+                "match_all": {}
+            },
+            "sort": [
+                {"date": "desc"},
+                {"_id": "desc"}
+            ]
+        }
+
+        if ts is not None:
+            query['search_after'] = [ts, id]
+        try:
+            res = es.search(index=index_name, body=query)
+            print(res)
+            return {'transactions' : res['hits']['hits']}, 200, \
+                response_type
+        except Exception as e:
+            return json.dumps({'message' : str(e)}), 500, response_type  
+        return "Hello"
     if request.method == 'POST':
         data = json.loads(request.data.decode('utf-8'))
         try:
@@ -40,8 +79,23 @@ def transactions():
             return json.dumps({'message' : str(e)}), 500, response_type  
 
         return json.dumps({'message' : 'success'}), 200, response_type  
-    else:
-        return 'Not allowed!'
+    if request.method == 'DELETE':
+        ids = request.args.get('ids')
+        ids = ids.split(",")
+        actions = [
+            {
+                "_index" : index_name,
+                "_id" : id,
+                "_op_type" : "delete"
+            }        
+            for id in ids    
+        ]
+
+        try:
+            helpers.bulk(es, actions)
+            return json.dumps({'message' : 'success'}), 200, response_type  
+        except Exception as e:
+            return json.dumps({'message' : str(e)}), 500, response_type
     
 
 if __name__ == '__main__':
